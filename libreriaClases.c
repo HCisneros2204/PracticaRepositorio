7a5ea61330d2602b02bb5d880fefe1c8a1d0a253
Fabricio <yaomingcisneros@gmail.com>
2021-05-13 17:24:41 -0500

    nueva linea y error en la linea 5

--- libreriaClases.c
+++ libreriaClases.c
@@ -1,4 +1,4 @@
-//esta linea es para cambiar las subrama
+//esta linea es para cambiar las subrama del git 
 // esta linea es para mis otros cambios
 //esta es la tercera linea 
 //Una linea sin problema en el merge (fusion de archivos)
