36fdc57638e9799708ad2d7ddc47e855feac61f6
Fabricio <yaomingcisneros@gmail.com>
2021-05-13 17:19:02 -0500

    nueva linea y error en la linea 5

--- libreriaClases.c
+++ libreriaClases.c
@@ -1,3 +1,5 @@
 //esta linea es para cambiar las subrama
 // esta linea es para mis otros cambios
 //esta es la tercera linea 
+//Una linea sin problema en el merge (fusion de archivos)
+//error Merge
